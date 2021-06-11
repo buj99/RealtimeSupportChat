4cca096cc671dade8b9f2c28a506354b1582c1fe
Bujenita Claudiu Dumitrel <bujenitaclaudiu@gmail.com>
2021-06-11 18:52:17 +0300

    .

--- backend/routes/asignchatRoute.js
+++ backend/routes/asignchatRoute.js
@@ -31,72 +31,83 @@ const asignChat = async (pathParams, req, res) => {
     );
     return;
   }
-  //check if unique auth admin key is corect
-  const verify = authChatAdminTokenVerify(req, res);
-  if (verify.succes) {
-    //check if admin exist in DB
-    Admin.findById(verify.verified._id)
-      .then((admin) => {
-        console.log(admin);
-        if (!admin) {
-          //admin doesn't exist
-          // res.statusCode = 400;
-          res.writeHead(400, headers);
-          res.end(
-            JSON.stringify({ message: "This admin key is not eligible" })
-          );
-          return;
-        }
-        //create a new conversation
-        const newConversation = Conversation({
-          admin_id: verify.verified._id,
-        });
-        newConversation
-          .save()
-          .then((data) => {
-            //add created chat to admin chat list
-            admin.conversations = [
-              ...admin.conversations,
-              { _id_chat: data._id },
-            ];
-            admin
-              .save()
-              .then(() => {
-                //create token identifyer for chat
-                const token = jwt.sign(
-                  { _id: data._id },
-                  process.env.TOKEN_SECRET_CHAT_IDENTIFIER
-                );
-                //save acces token for admin chat acces
-                data.admin_acces_token = token;
-                data.save().catch((error) => console.log(error));
-                //send response to client
-                // res.stautsCode = 200;
-                res.writeHead(200, headers);
-                res.end(JSON.stringify({ auth_chat: token }));
-              })
-              .catch((error) => {
-                //error when trying to add the new created chat to admin  chat list
-                console.log(error);
-                // res.statusCode = 500;
-                res.writeHead(500, headers);
-                res.end(JSON.stringify({ message: "Something went rong ." }));
-              });
-          })
-          .catch((error) => {
-            //error when tring to save in database the new chat
-            console.log(error);
-            // res.statusCode = 500;
-            res.writeHead(500, headers);
-            res.end(JSON.stringify({ message: "Something went rong ." }));
+  //get the body data from the request
+  let body = "";
+  req.on("data", (partData) => {
+    body = body + partData;
+  });
+  req.on("end", () => {
+    var parsedBody = JSON.parse(body);
+
+    //check if unique auth admin key is corect
+    const verify = authChatAdminTokenVerify(req, res);
+    if (verify.succes) {
+      //check if admin exist in DB
+      Admin.findById(verify.verified._id)
+        .then((admin) => {
+          console.log(admin);
+          if (!admin) {
+            //admin doesn't exist
+            // res.statusCode = 400;
+            res.writeHead(400, headers);
+            res.end(
+              JSON.stringify({ message: "This admin key is not eligible" })
+            );
+            return;
+          }
+          //create a new conversation
+          const newConversation = Conversation({
+            admin_id: verify.verified._id,
+            name: parsedBody.name,
+            photo_link: parsedBody.photo_link,
           });
-      })
-      .catch((error) => {
-        // res.statusCode = 500;
-        res.writeHead(500, headers);
-        res.end(JSON.stringify({ message: "Something went rong ." }));
-      });
-  }
+          newConversation
+            .save()
+            .then((data) => {
+              //add created chat to admin chat list
+              admin.conversations = [
+                ...admin.conversations,
+                { _id_chat: data._id },
+              ];
+              admin
+                .save()
+                .then(() => {
+                  //create token identifyer for chat
+                  const token = jwt.sign(
+                    { _id: data._id },
+                    process.env.TOKEN_SECRET_CHAT_IDENTIFIER
+                  );
+                  //save acces token for admin chat acces
+                  data.admin_acces_token = token;
+                  data.save().catch((error) => console.log(error));
+                  //send response to client
+                  // res.stautsCode = 200;
+                  res.writeHead(200, headers);
+                  res.end(JSON.stringify({ auth_chat: token }));
+                })
+                .catch((error) => {
+                  //error when trying to add the new created chat to admin  chat list
+                  console.log(error);
+                  // res.statusCode = 500;
+                  res.writeHead(500, headers);
+                  res.end(JSON.stringify({ message: "Something went rong ." }));
+                });
+            })
+            .catch((error) => {
+              //error when tring to save in database the new chat
+              console.log(error);
+              // res.statusCode = 500;
+              res.writeHead(500, headers);
+              res.end(JSON.stringify({ message: "Something went rong ." }));
+            });
+        })
+        .catch((error) => {
+          // res.statusCode = 500;
+          res.writeHead(500, headers);
+          res.end(JSON.stringify({ message: "Something went rong ." }));
+        });
+    }
+  });
 };
 
 module.exports.asignChat = asignChat;
