c35e2a200f566d9480dfe1f286c229e71697aa1d
alexxandra21 <conturalexandra@gmail.com>
2021-06-17 23:05:29 +0300

    Codeexample refresh works+style

--- frontend/private/static/js/views/Codeexample.js
+++ frontend/private/static/js/views/Codeexample.js
@@ -13,16 +13,34 @@ async getHTML() {
     return `
     <div class="codeex-container">
     <h1>To get your own personalized chat for your website just add the next code.</h1>
+    
     <h2>To your html:</h2>
-    <div><p>
+    <div><p class="codeex-p">
         insert code here
     </p></div>
+
     <h2>To your css:</h2>
-    <div><p>
-        insert code here
+    <div><p class="codeex-p">
+    .chat-container {
+        display: grid;
+        grid: "search-container chat-title" 2fr "conversation-list chat-message-list" 20fr "conversation-list chat-form" 3fr / 1fr 4fr;
+        width: 98%;
+        height: 95vh;
+        background-color: #fff;
+        border-radius: 10px;
+      }      
+@media all and (max-width: 520px) {
+    .chat-container {
+      width: 100%;
+      height: 100%;
+      border-radius: 0;
+      display: grid;
+      grid: "search-container chat-title" 2fr "conversation-list chat-message-list" 20fr "conversation-list chat-form" 3fr / 0px 4fr;
+    }}
     </p></div>
+
     <h2>To your js:</h2>
-    <div><p>
+    <div><p class="codeex-p">
         insert code here
     </p></div>
     <button class="back-btn" onclick="history.back()" >Back</button>
